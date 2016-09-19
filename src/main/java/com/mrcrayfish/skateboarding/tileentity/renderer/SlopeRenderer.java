0beef23a06fc98e08f39f33501c279d84b7ef25e
MrCrayfish <mrcrayfish@hotmail.com>
2016-09-19 17:35:22 +0930

    Changed textures and added metal detail to kicker

--- src/main/java/com/mrcrayfish/skateboarding/tileentity/renderer/SlopeRenderer.java
+++ src/main/java/com/mrcrayfish/skateboarding/tileentity/renderer/SlopeRenderer.java
@@ -18,11 +18,12 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.EnumSkyBlock;
 import net.minecraft.world.World;
+import scala.languageFeature.postfixOps;
 
-public class SlopeRenderer extends TileEntitySpecialRenderer<TileEntitySlope> 
-{
-	private static final ResourceLocation TEXTURE = new ResourceLocation("textures/blocks/hardened_clay.png");
-	
+public class SlopeRenderer extends TileEntitySpecialRenderer<TileEntitySlope> {
+	private static final ResourceLocation METAL_TEXTURE = new ResourceLocation("textures/blocks/stone_slab_top.png");
+	private static final ResourceLocation WOOD_TEXTURE = new ResourceLocation("textures/blocks/hardened_clay_stained_red.png");
+
 	@Override
 	public void renderTileEntityAt(TileEntitySlope te, double x, double y, double z, float partialTicks, int destroyStage) 
 	{
@@ -37,10 +38,11 @@ public class SlopeRenderer extends TileEntitySpecialRenderer<TileEntitySlope>
 			GlStateManager.translate(0, (int) (meta / 4) * 0.5, 0);
 			GlStateManager.disableLighting();
 			
-			Minecraft.getMinecraft().getTextureManager().bindTexture(TEXTURE);
 			Tessellator tessellator = Tessellator.getInstance();
 			VertexBuffer buffer = tessellator.getBuffer();
 			GlStateManager.color(1.0F, 1.0F, 1.0F);
+			
+			Minecraft.getMinecraft().getTextureManager().bindTexture(WOOD_TEXTURE);
 			
 			// Sides
 			if((meta / 4) > 0)
@@ -86,7 +88,7 @@ public class SlopeRenderer extends TileEntitySpecialRenderer<TileEntitySlope>
 			buffer.pos(1, 0.5, 0).tex(0, 1).endVertex();
 			tessellator.draw();
 			
-			getLighting(te.getWorld(), te.getPos(), EnumFacing.DOWN, 1);
+			getLighting(te.getWorld(), te.getPos(), EnumFacing.DOWN, 3);
 			buffer.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_TEX);
 			buffer.pos(1, 0, 0).tex(0, 1).endVertex();
 			buffer.pos(1, 0, 1).tex(0, 0).endVertex();
@@ -101,12 +103,27 @@ public class SlopeRenderer extends TileEntitySpecialRenderer<TileEntitySlope>
 			buffer.pos(1, 0.5, 1).tex(1, 0).endVertex();
 			buffer.pos(1, 0, 1).tex(1, 0.5).endVertex();
 			tessellator.draw();
+			
+			Minecraft.getMinecraft().getTextureManager().bindTexture(METAL_TEXTURE);
+			
+			if(!(te.getWorld().getBlockState(te.getPos().offset(state.getValue(BlockSlope.FACING).getOpposite())).getBlock() instanceof BlockSlope) 
+			&& !(te.getWorld().getBlockState(te.getPos().offset(state.getValue(BlockSlope.FACING).getOpposite()).down()).getBlock() instanceof BlockSlope)
+			&& meta / 4 != 1)
+			{
+				GlStateManager.translate(0, 0.0001, 0);
+				getLighting(te.getWorld(), te.getPos(), EnumFacing.UP, 0);
+				buffer.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_TEX);
+				buffer.pos(0, 0, 0).tex(1, 1).endVertex();
+				buffer.pos(0, 0, 1).tex(1, 0).endVertex();
+				buffer.pos(0.25, 0.125, 1).tex(0.75, 0).endVertex();
+				buffer.pos(0.25, 0.125, 0).tex(0.75, 1).endVertex();
+				tessellator.draw();
+			}
 		}
 		GlStateManager.popMatrix();
 	}
-	
-	public void getLighting(World world, BlockPos pos, EnumFacing facing, int modify) 
-	{
+
+	public void getLighting(World world, BlockPos pos, EnumFacing facing, int modify) {
 		pos = pos.offset(facing);
 		int light = world.getLightFor(EnumSkyBlock.SKY, pos) - modify;
 		int torch = world.getLightFor(EnumSkyBlock.BLOCK, pos);
