8c2aed7703cbb20a5859f6b7fdfb8041aaf4a848
MrCrayfish <mrcrayfish@hotmail.com>
2016-10-02 22:28:59 +1030

    Updated version

--- src/main/java/com/mrcrayfish/skateboarding/Reference.java
+++ src/main/java/com/mrcrayfish/skateboarding/Reference.java
@@ -4,7 +4,7 @@ public class Reference
 {
 	public static final String MOD_ID = "csm";
 	public static final String MOD_NAME = "MrCrayfish's Skateboarding Mod";
-	public static final String MOD_VERSION = "1.0";
+	public static final String MOD_VERSION = "0.0.2";
 	public static final String CLIENT_PROXY_CLASS = "com.mrcrayfish.skateboarding.proxy.ClientProxy";
 	public static final String SERVER_PROXY_CLASS = "com.mrcrayfish.skateboarding.proxy.CommonProxy";
 }
